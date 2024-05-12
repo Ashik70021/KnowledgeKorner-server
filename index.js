90173d3dd3e3d6d3fab9f54a370fac28d4ad9aad
Ashik70021 <ashik70021@gmail.com>
2024-05-12 22:26:17 +0600

    implement query to find single blog using id

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run() {
             res.send(result)
         })
 
+        // Get single blog data by id
+        app.get('/blog/:id', async (req, res) =>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await blogsCollection.findOne(query)
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
